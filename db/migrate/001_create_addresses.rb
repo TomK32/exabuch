e449ded7d924b7a0aaffb38a0d26e96a79b3b0f9
novalis <novalis@9dc2ab4d-e83a-0410-94c7-bf54c6f31d53>
2007-09-22 13:07:44 +0000

    Added addresses
    git-svn-id: http://exabuch.googlecode.com/svn/trunk@3 9dc2ab4d-e83a-0410-94c7-bf54c6f31d53

---
+++ db/migrate/001_create_addresses.rb
@@ -0,0 +1,16 @@
+class CreateAddresses < ActiveRecord::Migration
+  def self.up
+    create_table :addresses do |t|
+			t.column :company, :string
+			t.column :name, :string
+			t.column :street, :string
+			t.column :street_number, :string
+			t.column :postcode, :string
+			t.column :country, :string
+    end
+  end
+
+  def self.down
+    drop_table :addresses
+  end
+end

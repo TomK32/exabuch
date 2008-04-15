cea79c41130a389ad266d2e4d7019d83570ac3ec
TomK32 <tomk32@localhost.(none)>
2008-04-15 11:23:03 +0200

    migrated to rails 2.0.2 included active_scaffold

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-
+  
   def to_currency(val=0, precision=2, unit="€", separator=",", delimiter=".")
     number_with_delimiter(number_with_precision(val, precision), delimiter, separator)+" "+unit
   end
@@ -33,5 +33,4 @@ module ApplicationHelper
     end
     super
   end
-  
 end

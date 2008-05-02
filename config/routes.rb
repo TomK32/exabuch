6c5a637aa7abf877c85aed459218de5b054c30d6
TomK32 <tomk32@tomk32.de>
2008-05-02 12:26:18 +0200

    ok, must be  as string, not a symbol

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :addresses
   map.resources :invoices, :has_many => :items, :member => {:pdf => :get}
 
-  map.root :controller => :invoices
+  map.root :controller => 'invoices'
 
   # See how all your routes lay out with "rake routes"
 

4bd086faa87e8c8d9e80e531580334fc1cc5cdaa
TomK32 <tomk32@tomk32.de>
2008-05-10 07:53:25 +0200

    very small fix for price

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,13 @@ class Item < ActiveRecord::Base
 
   belongs_to :invoice
   validates_presence_of :tax
-  
+
   def tax_amount
     (self.price * self.tax) / 100.0 * self.amount
   end
 
   def price_amount
-    return price * self.amount
+    return self.price * self.amount
   end
   def price
     return 0 if self.price_in_cents.nil?

f5dc7c257f799060d285eaafd560aebe46511cdf
TomK32 <tomk32@tomk32.de>
2008-05-10 07:52:54 +0200

    moved some code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,11 @@ class ItemsController < ApplicationController
     @item = Item.new
 
     respond_to do |format|
-      format.js
+      format.js do
+        # necessary to give each item a unique id and to have many of them in the form
+        @item = Item.new()
+        @item.id = Time.now.to_i
+      end
       format.html # new.html.erb
       format.xml  { render :xml => @item }
     end

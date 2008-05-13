107fe4b3f7f5e179092fdd07f185a011899c2a02
TomK32 <tomk32@tomk32.de>
2008-05-13 09:12:56 +0200

    stripping down to needed methods onyl

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,4 @@
 class ItemsController < ApplicationController
-  def index
-    @items = Item.find :all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @items }
-    end
-  end
-
-  def show
-    @item = Item.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @item }
-    end
-  end
-
   def new
     @item = Item.new
 
@@ -28,51 +10,6 @@ class ItemsController < ApplicationController
       end
       format.html # new.html.erb
       format.xml  { render :xml => @item }
-    end
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def create
-    @item = Item.new(params[:item])
-
-    respond_to do |format|
-      if @item.save
-        flash[:notice] = 'Item was successfully created.'
-        format.html { redirect_to(@item) }
-        format.xml  { render :xml => @item, :status => :created, :location => @item }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    @item = Item.find(params[:id])
-
-    respond_to do |format|
-      if @item.update_attributes(params[:item])
-        flash[:notice] = 'Item was successfully updated.'
-        format.html { redirect_to(@item) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-
-    respond_to do |format|
-      format.js { render :nothing }
-      format.html { redirect_to(items_url) }
-      format.xml  { head :ok }
     end
   end
 end

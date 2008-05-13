8219aca9bdecca15488c4024e33972340d14b6d5
TomK32 <tomk32@tomk32.de>
2008-05-13 13:12:41 +0200

    test for user addresses

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,14 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  # should have one user_address and two addresses (includes the own one)
+  def test_01_addresses
+    user1 = User.find 1
+    assert_equal 1, user1.addresses.count
+    assert_equal 1, user1.user_addresses.count
+    assert_not_equal user1.addresses.first, user1.user_addresses.first
+    assert_equal 'Wollewolle Inc., 21138 Berlin', user1.user_addresses.first.short_info
+    assert_equal Address.find(2).short_info, user1.addresses.first.short_info
   end
 end

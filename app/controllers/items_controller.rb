611ad8448cdc26cadb5fd22d0bfec5629529bbc2
TomK32 <tomk32@tomk32.de>
2008-05-13 09:13:29 +0200

    removed unnecessary line

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,6 @@ class ItemsController < ApplicationController
     respond_to do |format|
       format.js do
         # necessary to give each item a unique id and to have many of them in the form
-        @item = Item.new()
         @item.id = Time.now.to_i
       end
       format.html # new.html.erb
